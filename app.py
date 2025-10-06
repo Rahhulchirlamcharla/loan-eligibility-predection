55d98620d457c0ab47fb1ce7d71348ceb21309f3
Rahhul chirlamcharla <153748962+Rahhulchirlamcharla@users.noreply.github.com>
2025-10-06 20:36:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host="localhost",
     user="root",          # change if your MySQL user is different
-    password="Shreya$123",  # replace with your MySQL password
+    password="",  # replace with your MySQL password
     database="loanDB"
 )
 cursor = db.cursor()
